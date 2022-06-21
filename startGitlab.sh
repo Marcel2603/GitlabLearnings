070dabaa613b8203eefc00bcf7f192c5c2639b97
Marcel Herhold <www.marcel.herhold@googlemail.com>
2022-06-21 20:39:44 +0200

    Add script

--- startGitlab.sh
+++ startGitlab.sh
@@ -1,30 +1,5 @@
 #!/bin/bash
 
-dir=$(pwd)
-GITLAB_HOME=${dir}/gitlab
-
-mkdir -p ${GITLAB_HOME}
-
-docker run --detach \
-  --hostname gitlab.local \
-  --publish 443:443 --publish 80:80 --publish 22:22 \
-  --name gitlab \
-  --restart always \
-  --volume $GITLAB_HOME/config:/etc/gitlab \
-  --volume $GITLAB_HOME/logs:/var/log/gitlab \
-  --volume $GITLAB_HOME/data:/var/opt/gitlab \
-  --shm-size 256m \
-  --network bridge \
-  gitlab/gitlab-ee:latest
-
-docker ps gitlab
-
-docker exec -it gitlab grep 'Password:' /etc/gitlab/initial_root_password   
-
-docker volume create gitlab-runner-config
-
-docker run -d --name gitlab-runner --restart always \
-    -v /var/run/docker.sock:/var/run/docker.sock \
-    -v gitlab-runner-config:/etc/gitlab-runner \
-    --network bridge \
-    gitlab/gitlab-runner:latest
+docker-compose up -d
+sleep 120
+docker-compose exec gitlab grep 'Password:' /etc/gitlab/initial_root_password   
